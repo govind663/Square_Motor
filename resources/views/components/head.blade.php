75611a681f861fa40740dcdc0535d47eb20ccd6d
Abhishe G Jha <abhishek.j@peaceinfotech.com>
2024-06-24 18:29:42 +0530

    Square Motors | Version - 0.126

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -38,6 +38,6 @@
 <script src="{{ asset('assets/js/layout.js') }}" type="ecc6729bbed9073710614b89-text/javascript"></script>
 
 <!-- Toaster Message -->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/toaster/css/toastr.min.css') }}" />
 <script src="{{ asset('assets/toaster/js/jquery.min.js') }}"></script>
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/toaster/css/toastr.min.css') }}" />
 <script src="{{ asset('assets/toaster/js/toastr.min.js') }}"></script>
